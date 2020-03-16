439fa01df56e1d67d38aa60872dbeaa85b8d9b05
ohrim433 <57233121+ohrim433@users.noreply.github.com>
2020-03-16 19:43:40 +0200

    Added classnames

--- src/components/MovieTabs.jsx
+++ src/components/MovieTabs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const MovieTabs = (props) => {
     const {sort_by, updateSortBy} = props;
@@ -7,10 +8,10 @@ const MovieTabs = (props) => {
         updateSortBy(value);
     };
 
-    const getClassLink = value => {
-        return `nav-link ${sort_by === value ?
-            "active" : ''}`
-    };
+    const getClassLink = value =>
+        classNames("nav-link", {active: sort_by === value});
+    // return `nav-link ${sort_by === value ? "active" : ''}` // without classnames
+
     return (
         <ul className="tabs nav nav-pills">
             <li className="nav-item">
